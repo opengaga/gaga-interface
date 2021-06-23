cb9bc02ae8aabf24a95052b3bafd9a4bb54c6094
ZYHZZZ\18328 <1832819123@qq.com>
2021-06-23 21:27:48 +0800

    Update index.vue

--- src/views/multiple/index.vue
+++ src/views/multiple/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="collectible-wrap">
     <back-btns text="Manage collectible type" link="create" />
-    <div class="page-title">Create multiple collectible</div>
+    <div class="page-title">{{ $t('Create multiple collectible') }}</div>
     <div class="single-wrap">
       <mint-form :isMulti="true" :state="form" @submit="submit" />
       <div class="preview-wrap">
@@ -10,16 +10,19 @@
           <div v-if="form.preview">
             <img class="logo" src="@/assets/imgs/brandLogo.png" />
             <img class="preview-img" :src="form.preview" />
-            <div><span class="bold600">Auction</span> <span class="opa05">1 of 1</span></div>
-            <div class="wallet-bid">Place a bid</div>
+            <div
+              ><span class="bold600">{{ $t('Auction') }}</span>
+              <span class="opa05">1 of 1</span></div
+            >
+            <div class="wallet-bid">{{ $t('Place a bid') }}</div>
           </div>
-          <div v-else> Preview of your new collectible </div>
+          <div v-else> {{ $t('Preview of your new collectible') }} </div>
         </div>
       </div>
     </div>
     <a-modal
       :visible="visible"
-      title="Follow steps"
+      :title="$t('Follow steps')"
       :footer="null"
       :maskClosable="false"
       :destroyOnClose="true"
